6788e3ada3fbd529c1bd8f53478c0b68b3348dba
julfiker755 <julfiker755.bd@gmail.com>
2024-11-24 00:50:47 +0600

    project init

---
+++ src/app/(common)/page.tsx
@@ -0,0 +1,12 @@
+import { Button } from '@/components/ui/button'
+import React from 'react'
+
+function Rootpage() {
+  return (
+    <div className='text-3xl'>
+        <Button variant='softBg'>Submit Hare</Button>
+    </div>
+  )
+}
+
+export default Rootpage
---
+++ tailwind.config.ts
@@ -0,0 +1,24 @@
+import type { Config } from "tailwindcss";
+
+const config: Config = {
+  content: [
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  darkMode:'media', 
+  theme: {
+    container: {
+      center: true,
+      padding: {
+        DEFAULT: "1rem",
+        md: "2rem",
+        lg: "3rem",
+        xl: "4rem",
+        "2xl": "5rem",
+      },
+    },
+  },
+  plugins: [],
+};
+export default config;
