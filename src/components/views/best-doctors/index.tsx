7485bc979f416c194da0e911ba031fd764e42a02
julfiker755 <julfiker755.bd@gmail.com>
2025-03-20 22:12:38 +0600

    Best Doctor card design change

--- src/components/views/best-doctors/index.tsx
+++ src/components/views/best-doctors/index.tsx
@@ -1,13 +1,23 @@
 "use client";
-import React from "react";
 import { IconStarFilled } from "@tabler/icons-react";
 import { NoItemData, Title } from "@/components/reusable";
-import Image from "next/image";
 import { useGetAllDoctorQuery } from "@/redux/api/doctorApi";
-import { PlaceholderImg } from "@/lib/utils";
-import { Skeleton } from "@/components/ui";
-import Link from "next/link";
 import { Zoom } from "@/components/animation";
+import { PlaceholderImg } from "@/lib/utils";
+import { Button, Skeleton } from "@/components/ui";
+import Link from "next/link";
+import Image from "next/image";
+import React from "react";
+import { Rating, Star } from "@smastrom/react-rating";
+import "@smastrom/react-rating/style.css";
+
+const myStyles = {
+  itemShapes: Star,
+  activeFillColor: "white",
+  activeBoxColor: "#0e82fd",
+  inactiveFillColor: "white",
+  inactiveBoxColor: "#0e82fd7d",
+};
 
 export default function BestDoctors() {
   const { data, isLoading } = useGetAllDoctorQuery({});
@@ -18,7 +28,7 @@ export default function BestDoctors() {
         title="Best Doctor"
         text="Book online to connect with our trusted experts"
       ></Title>
-      <div className="grid grid-cols-1 gap-5 lg:grid-cols-4">
+      <div className="grid grid-cols-1 gap-4 lg:grid-cols-4">
         {isLoading ? (
           [...Array(4)].map((_, index) => (
             <div key={index} className="border border-gray-100 rounded-md p-2">
@@ -33,43 +43,71 @@ export default function BestDoctors() {
             </div>
           ))
         ) : data?.doctors?.length > 0 ? (
-          data?.doctors?.slice(0, 4).map((item: any) => (
-            <Link href={`/doctors/${item.id}`} key={item.id}>
-              <Zoom className="border rounded-md p-2">
-                <div className="relative">
-                  <Image
-                    src={
-                      item.profilePhoto !== null
-                        ? `${process.env.NEXT_PUBLIC_IMAGE_URL}/${item.profilePhoto}`
-                        : PlaceholderImg()
-                    }
-                    alt="77"
-                    className="m-auto relative rounded-sm"
-                    width={200}
-                    height={100}
-                    style={{
-                      width: "100%",
-                      display: "block",
-                      margin: "auto",
-                      maxWidth: "100%",
-                      height: "180px",
-                    }}
-                  ></Image>
-                </div>
-                <div className="py-2 flex justify-between">
-                  <ul>
-                    <li className="text-lg font-medium">{item.name}</li>
-                    <li className="text-gray-600 text-sm">
-                      {item.qualification}
-                    </li>
-                  </ul>
-                  <div className="bg-[#1C5B91] flex items-center gap-[2px] w-fit h-fit text-white px-2 rounded-md text-sm">
-                    <IconStarFilled size={16} />
-                    {item.averageRating}
-                  </div>
-                </div>
-              </Zoom>
-            </Link>
+          data?.doctors?.slice(0, 4)?.map((item: any) => (
+            <Zoom key={item?.id} className="border rounded-md px-2 pt-2">
+              <div className="relative">
+                <Image
+                  src={
+                    item?.profilePhoto !== null
+                      ? `${process.env.NEXT_PUBLIC_IMAGE_URL}/${item.profilePhoto}`
+                      : PlaceholderImg()
+                  }
+                  alt="77"
+                  className="m-auto relative rounded-sm"
+                  width={200}
+                  height={100}
+                  style={{
+                    width: "100%",
+                    display: "block",
+                    margin: "auto",
+                    maxWidth: "100%",
+                    height: "180px",
+                  }}
+                ></Image>
+              </div>
+              <div className="py-2">
+                <ul className="flex justify-between pb-1">
+                  <li>
+                    <Rating
+                      style={{ maxWidth: 115 }}
+                      value={item?.averageRating}
+                      itemStyles={myStyles}
+                      spaceBetween="small"
+                      halfFillMode="box"
+                      readOnly
+                    />
+                  </li>
+                  <li className="bg-[#8d2ee6] flex items-center gap-[2px] w-fit h-fit text-white px-2 text-sm rounded-[3px]">
+                    {item?.averageRating}
+                  </li>
+                </ul>
+                <ul>
+                  <li className="text-lg font-medium">{item.name}</li>
+                  <li className="text-gray-700 text-xs">
+                    {item?.qualification}&nbsp;-&nbsp;
+                    {item?.specialities[0]?.title || "Cardiology"}
+                  </li>
+                </ul>
+                <hr className="my-[6px]" />
+                <ul className="flex justify-between">
+                  <li>
+                    <Link href={`/doctors/${item.id}`} key={item.id}>
+                      <Button
+                        className="rounded-full bg-[#012047] hover:bg-[#012047]/90"
+                        size="sm"
+                      >
+                        View Profile
+                      </Button>
+                    </Link>
+                  </li>
+                  <li>
+                    <Button className="rounded-full" size="sm">
+                      Book Now
+                    </Button>
+                  </li>
+                </ul>
+              </div>
+            </Zoom>
           ))
         ) : (
           <div className="col-span-4">
