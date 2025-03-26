726c8b1438e4175d12588f20028080ed66ce7662
julfiker755 <julfiker755.bd@gmail.com>
2025-03-27 00:03:49 +0600

    assets anathor image remove

--- src/assets/index.ts
+++ src/assets/index.ts
@@ -3,34 +3,35 @@ import profileImg from "@/assets/works/profile.svg";
 import bookImg from "@/assets/works/book.svg";
 import receiveImg from "@/assets/works/solution.svg";
 import workImg from "@/assets/works/workimg.jpg";
-import partners1 from "@/assets/partners/partners-1.svg"
-import partners2 from "@/assets/partners/partners-2.svg"
-import partners3 from "@/assets/partners/partners-3.svg"
-import partners4 from "@/assets/partners/partners-4.svg"
-import partners5 from "@/assets/partners/partners-5.svg"
-import partners6 from "@/assets/partners/partners-6.svg"
-import logo from "@/assets/logo.svg"
-import logo2 from "@/assets/logo2.svg"
-import blogProImg1 from "@/assets/blog/profile1.jpg"
-import blogProImg2 from "@/assets/blog/profile2.jpg"
-import blogProImg3 from "@/assets/blog/profile3.jpg"
-import blogProImg4 from "@/assets/blog/profile4.jpg"
-import blogProImg5 from "@/assets/blog/profile5.jpg"
-import blogProImg6 from "@/assets/blog/profile6.jpg"
-import blog1Img from "@/assets/blog/blog1.jpg"
-import blog2Img from "@/assets/blog/blog2.jpg"
-import blog3Img from "@/assets/blog/blog3.jpg"
-import blog4Img from "@/assets/blog/blog4.jpg"
-import blog5Img from "@/assets/blog/blog5.jpg"
-import blog6Img from "@/assets/blog/blog6.jpg"
-
-
-
+import partner1 from "@/assets/partners/partners-1.svg";
+import partner2 from "@/assets/partners/partners-2.svg";
+import partner3 from "@/assets/partners/partners-3.svg";
+import partner4 from "@/assets/partners/partners-4.svg";
+import partner5 from "@/assets/partners/partners-5.svg";
+import partner6 from "@/assets/partners/partners-6.svg";
+import partner7 from "@/assets/partners/partners-7.svg";
+import partner8 from "@/assets/partners/partners-8.svg";
+import partner9 from "@/assets/partners/partners-9.svg";
+import partner10 from "@/assets/partners/partners-10.svg";
+import blogProImg1 from "@/assets/blog/profile1.jpg";
+import blogProImg2 from "@/assets/blog/profile2.jpg";
+import blogProImg3 from "@/assets/blog/profile3.jpg";
+import blogProImg4 from "@/assets/blog/profile4.jpg";
+import blogProImg5 from "@/assets/blog/profile5.jpg";
+import blogProImg6 from "@/assets/blog/profile6.jpg";
+import blog1Img from "@/assets/blog/blog1.jpg";
+import blog2Img from "@/assets/blog/blog2.jpg";
+import blog3Img from "@/assets/blog/blog3.jpg";
+import blog4Img from "@/assets/blog/blog4.jpg";
+import blog5Img from "@/assets/blog/blog5.jpg";
+import blog6Img from "@/assets/blog/blog6.jpg";
+import logo from "@/assets/logo.svg";
+import logo2 from "@/assets/logo2.svg";
 
 const assets = {
   images: {
-    logo:logo,
-    logo2:logo2,
+    logo: logo,
+    logo2: logo2,
     solution: {
       workImg,
       findImg,
@@ -38,29 +39,33 @@ const assets = {
       bookImg,
       receiveImg,
     },
-  partners:{
-     partners1,
-     partners2,
-     partners3,
-     partners4,
-     partners5,
-     partners6,
-  }
   },
-blog:{
-  blog1Img:blog1Img,
-  blog2Img:blog2Img,
-  blog3Img:blog3Img,
-  blog4Img:blog4Img,
-  blog5Img:blog5Img,
-  blog6Img:blog6Img,
-  ProImg1:blogProImg1,
-  ProImg2:blogProImg2,
-  ProImg3:blogProImg3,
-  ProImg4:blogProImg4,
-  ProImg5:blogProImg5,
-  ProImg6:blogProImg6,
-}
+  partner: {
+    partner1,
+    partner2,
+    partner3,
+    partner4,
+    partner5,
+    partner6,
+    partner7,
+    partner8,
+    partner9,
+    partner10,
+  },
+  blog: {
+    blog1Img: blog1Img,
+    blog2Img: blog2Img,
+    blog3Img: blog3Img,
+    blog4Img: blog4Img,
+    blog5Img: blog5Img,
+    blog6Img: blog6Img,
+    ProImg1: blogProImg1,
+    ProImg2: blogProImg2,
+    ProImg3: blogProImg3,
+    ProImg4: blogProImg4,
+    ProImg5: blogProImg5,
+    ProImg6: blogProImg6,
+  },
 };
 
 export default assets;
--- src/components/views/our-partners/index.tsx
+++ src/components/views/our-partners/index.tsx
@@ -6,12 +6,16 @@ import Image from "next/image";
 
 export default function OurPartners() {
   const partnerItem = [
-    { id: 1, img: assets.images.partners.partners1 },
-    { id: 2, img: assets.images.partners.partners2 },
-    { id: 3, img: assets.images.partners.partners3 },
-    { id: 4, img: assets.images.partners.partners4 },
-    { id: 5, img: assets.images.partners.partners5 },
-    { id: 6, img: assets.images.partners.partners6 },
+    { id: 1, img: assets.partner.partner1 },
+    { id: 2, img: assets.partner.partner2 },
+    { id: 3, img: assets.partner.partner3 },
+    { id: 4, img: assets.partner.partner4 },
+    { id: 5, img: assets.partner.partner5 },
+    { id: 6, img: assets.partner.partner6 },
+    { id: 7, img: assets.partner.partner7 },
+    { id: 8, img: assets.partner.partner8 },
+    { id: 9, img: assets.partner.partner9 },
+    { id: 10, img: assets.partner.partner10 },
   ];
   return (
     <div className="container">
@@ -20,13 +24,17 @@ export default function OurPartners() {
         text="Explore Our Trusted Clinic Partners and Connect with Experts Today"
       />
       <Marquee>
-        <div className="flex gap-9">
+        <div className="flex gap-x-4">
           {partnerItem.map((item, index) => (
             <div key={index} className="border w-fit px-3 py-2 rounded-md">
               <Image
                 width={130}
                 height={100}
-                alt={item.img.toString()}
+                style={{
+                  height: "30px",
+                }}
+                className="text-black"
+                alt={item?.img?.toString()}
                 src={item.img}
               />
             </div>
