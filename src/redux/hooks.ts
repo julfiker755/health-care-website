1690c841d98fc42856460921eec2eeee97e51929
julfiker755 <julfiker755.bd@gmail.com>
2025-07-15 00:26:35 +0600

    hooks error bug sloved

--- src/redux/hooks.ts
+++ src/redux/hooks.ts
@@ -1,10 +1,10 @@
 import { useDispatch, useSelector } from 'react-redux'
-import type { RootState, AppDispatch } from './store'
 import { useEffect, useState } from 'react'
+import { AppDispatch, AppState } from './store'
 
 
 export const useAppDispatch = useDispatch.withTypes<AppDispatch>()
-export const useAppSelector = useSelector.withTypes<RootState>()
+export const useAppSelector = useSelector.withTypes<AppState>()
 
 
 
